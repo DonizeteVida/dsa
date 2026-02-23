a9d309c5dc86dbac6e81ef10f953cd54beffa7b6
DonizeteVida <donizetevida1998@outlook.com>
2026-02-23 22:51:00 +0100

    Add another substring implementation

--- c/exercises/002_max_length.c
+++ c/exercises/002_max_length.c
@@ -5,6 +5,31 @@
 
 #define maxOf(a, b) ((a) > (b) ? a : b)
 
+static int maximum_length_substring(char* s) {
+    #define MAX_OCCURRENCES 2
+
+    int left = 0, right = 0, max_length = 0, length = 0;
+
+    unsigned int size = strlen(s);
+    unsigned char occurrences['z' - 'a' + 1] = {0};
+
+    for (right = 0; right < size; right++) {
+        char c = s[right];
+        while (occurrences[c - 'a'] >= MAX_OCCURRENCES) {
+            occurrences[s[left++] - 'a']--;
+            if (length > max_length) max_length = length;
+            length--;
+        }
+
+        occurrences[c - 'a']++;
+        length++;
+    }
+
+    if (length > max_length) max_length = length;
+
+    return max_length;
+}
+
 static int max_substring_by_count(char *str, int count, char **ret)
 {
     int left = 0, right = 0;
@@ -27,7 +52,7 @@ static int max_substring_by_count(char *str, int count, char **ret)
 
     *ret = str + left;
 
-    return right - left;
+    return right - left + 1;
 }
 
 int main(int argc, char const *argv[])
@@ -66,6 +91,7 @@ int main(int argc, char const *argv[])
 
         max = max_substring_by_count(str, 1, &ret);
         assert(strncmp(ret, "zabcd", max) == 0);
+        assert(maximum_length_substring(str) == max);
     }
 
     return 0;
